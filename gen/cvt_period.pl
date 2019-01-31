645e41f81b607500b964d47d14aeadae75837f06
renato <renato@localhost.localdomain>
2019-01-31 23:35:30 +0000

    period_phase table

---
+++ gen/cvt_period.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $clock_pal  = 7093789.2;
+my $clock_ntsc = 7159090.5;
+
+#my @periods = (428, 404, 381, 360, 339, 320, 302, 285, 269, 254, 240, 226);
+my @periods = (1..2024);
+
+foreach(@periods)
+{
+	my $sps1 = $clock_pal/(2*$_);
+	#	my $sps2 = $clock_ntsc/(2*$_);
+	#
+	#	my $ka = 8287.13691588785;
+	my $k = 8287.13691588785/22050;
+
+
+	my $d = $sps1/22050;
+	my $phase = int($d*0x1000+0.5);
+	#	print "$_ => $sps1, $d\n";
+
+	printf("%5d => %10.4f, %10.4f %08X\n", $_, $sps1, $d, $phase);
+}
+
+
